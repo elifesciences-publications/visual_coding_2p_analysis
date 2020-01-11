e53dc2335a0ccedcc2f77a18ed8f6824e1777e0f
matchings <marinag@alleninstitute.org>
2020-01-10 17:56:32 -0800

    update to platform paper parameters

--- visual_coding_2p_analysis/l0_analysis.py
+++ visual_coding_2p_analysis/l0_analysis.py
@@ -49,19 +49,13 @@ class L0_analysis:
 
     """
     def __init__(self, dataset,
-                       event_min_size=1., median_filter_1=5401, median_filter_2=101, halflife_ms=None, cache_directory=None,
-                       sample_rate_hz=30, genotype='Unknown', L0_constrain=True, use_cache=False, use_bisection=False):
+                       event_min_size=2., noise_scale=.1, median_filter_1=5401, median_filter_2=101, halflife_ms=None, cache_directory=None,
+                       sample_rate_hz=30, genotype='Unknown', L0_constrain=False, use_cache=True, use_bisection=True):
 
         # manifest_file = core.get_manifest_path()
         manifest_file = None
         self.experiment_id = dataset.experiment_id
-
-        self.cache_directory = cache_directory
-        self.use_cache = use_cache
-        self.use_bisection = use_bisection
-        self.median_filter_1 = median_filter_1
-        self.median_filter_2 = median_filter_2
-        self.L0_constrain = L0_constrain
+        
 
         if type(dataset) is int:
             if manifest_file is None:
@@ -79,43 +73,29 @@ class L0_analysis:
             print('got corrected traces')
         except:
             self.metadata = {'genotype':genotype, 'ophys_experiment_id':999}
-            self._dff_traces = dataset
-            self.num_cells = self._dff_traces[0]
-            num_small_baseline_frames = []
-            noise_stds = []
+            self.corrected_fluorescence_traces = dataset
 
-            for dff in self._dff_traces:
-                sigma_dff = self.noise_std(dff)
-                noise_stds.append(sigma_dff)
-
-                # short timescale detrending
-                tf = medfilt(dff, self.median_filter_2)
-                tf = np.minimum(tf, 2.5*sigma_dff)
-                dff -= tf
-                self.print('.', end='', flush=True)
-            self._noise_stds = noise_stds
-            self._num_small_baseline_frames = num_small_baseline_frames
-
-
+        self.num_cells = self.corrected_fluorescence_traces.shape[0]
         self.sample_rate_hz = sample_rate_hz
         self.event_min_size = event_min_size
+        self.noise_scale = noise_scale
 
         if halflife_ms is None:
             self.halflife = self.get_halflife()
         else:
             self.halflife = halflife_ms
 
-        self.use_cache = use_cache
-        self.use_bisection = use_bisection
-        self.median_filter_1 = median_filter_1
-        self.median_filter_2 = median_filter_2
-        self.L0_constrain = L0_constrain
         if cache_directory is None: 
             self.cache_directory = core.get_cache_path()
         else: 
             self.cache_directory = cache_directory
         
-# 
+        self.use_cache = use_cache
+        self.use_bisection = use_bisection
+        self.median_filter_1 = median_filter_1
+        self.median_filter_2 = median_filter_2
+        self.L0_constrain = L0_constrain
+
         self._noise_stds = None
         self._num_small_baseline_frames = None
         self._dff_traces = None
@@ -123,7 +103,7 @@ class L0_analysis:
 
         self._gamma = None
         self.lambdas = []
-        # self.l0_func = None
+        self.l0_func = None
 
     # @property
     def l0(self, dff, gamma, l, constraint):                                                                                                                          
@@ -300,7 +280,7 @@ class L0_analysis:
                     if self.use_bisection:
                         (tmp, l) = self.bisection(tmp, self.dff_traces[1][n], self.event_min_size)
                     else:
-                        (tmp, l) = self.bracket(tmp, self.dff_traces[1][n], 0, 1, .0001, self.event_min_size)
+                        (tmp, l) = self.bracket(tmp, self.dff_traces[1][n], 0, 10*self.noise_scale, .0001, self.event_min_size)
 
 
                     events.append(tmp)
